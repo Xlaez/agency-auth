b4abaaf48d1a97d2de9f90e55a0d4bb0120c51f9
orion <carlasparsh@gmail.com>
2021-12-10 12:56:35 +0100

    authentication fix

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -10,6 +10,7 @@ exports.signup = (req, res) => {
     const user = new User({
         username: req.body.username,
         email: req.body.email,
+        fullname:req.body.fullname,
         password: bcrypt.hashSync(req.body.password, 8)
     });
 
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose")
 const User = mongoose.model(
     "User",
     new mongoose.Schema({
-        username: String,
-        email: String,
-        password: String,
+        username: {type: String, required:[true, 'enter a valid username']},
+        fullname: {type: String, required:[true, 'enter a fullname']},
+        email: {type: String, required:[true, 'enter an email ']},
+        password: {type: String, required:[true, 'enter a password ']},
+
         roles: [{
             type: mongoose.Schema.Types.ObjectId,
             ref: "Role"
