ae5ecd4d750270dd4dbd80291b66c34970b1d61a
Usenmfon <usenmfonuko@gmail.com>
2022-01-17 16:13:58 +0100

    updated auth jwt

---
+++ app/routes/user.route.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const router = express.Router()
+
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    editUser,
+} =  require("../controllers/user.controller");
+
+router.route('/api/all/user').get(getUser)
+router.route('/api/user/:id').get(getSingleUser).put(editUser)
+router.route('/api/user').post(createUser)
+
+module.exports = router
