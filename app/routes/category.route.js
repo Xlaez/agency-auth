abdb1c2cde784511a1c3c90d048681d976e5968b
Usenmfon <usenmfonuko@gmail.com>
2022-01-24 14:05:41 +0100

    created category route

---
+++ app/routes/category.route.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+    getCategories,
+    createCategory,
+    editCategory,
+    deleteCategory,
+} = require("../controllers/category.controller")
+
+router.route('/').get(getCategories)
+router.route('/create').post(createCategory)
+router.route('/:id').put(editCategory).delete(deleteCategory)
